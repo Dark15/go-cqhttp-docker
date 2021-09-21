f091dd406c29ba1d209a7b44b3aed6ec1edc8a03
Starry <starrycat15@gmail.com>
2021-09-21 15:42:20 +0800

    Bug fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,16 @@ init_config() {
   for ITEM in $SERVERS; do
     if [ "$ITEM" = "http" ]; then
       yq e -i "
-      .servers[0].disabled = false | 
-      .servers[0].post[0].url = strenv(HTTP_POST_URL) | 
-      .servers[0].post[0].secret = strenv(HTTP_POST_SECRET)
+      .servers[0].http.disabled = false | 
+      .servers[0].http.post[0].url = strenv(HTTP_POST_URL) | 
+      .servers[0].http.post[0].secret = strenv(HTTP_POST_SECRET)
       " ./config.yml
     elif [ "$ITEM" = "ws" ]; then
-      yq e -i ".servers[1].disabled = false" ./config.yml
+      yq e -i ".servers[1].ws.disabled = false" ./config.yml
     elif [ "$ITEM" = "ws-reverse" ]; then
       yq e -i "
-      .servers[2].disabled = false | 
-      .servers[2].universal = strenv(WS_REVERSE_UNIVERSAL)
+      .servers[2].ws-reverse.disabled = false | 
+      .servers[2].ws-reverse.universal = strenv(WS_REVERSE_UNIVERSAL)
       " ./config.yml
     fi
   done
